e3f883df14a4afb1574102b402e7ec1c1b2c70c9
tanakatakamasa-l <74941080+tanakatakamasa-l@users.noreply.github.com>
2022-05-07 23:39:08 +0900

    タグ機能追加　エラー発生
    NameError　編集ページでエラー発生
      <%= tag.check_box class: 'form-check-input' %>　が引っかかっている？
    上記を消すと通る、

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,20 +2,21 @@ class BoardsController < ApplicationController
   before_action :set_target_board, only: %i[show edit update destroy]
 
   def index
-    @boards = Board.page(params[:page])
+    @boards = params[:tag_id].present? ? Tag.find(params[:tag_id]).boards : Board.all
+    @boards = @boards.page(params[:page])
   end
 
   def new
-    @board = Board.new
+    @board = Board.new(flash[:board])
   end
-  
+
   def create
     board = Board.new(board_params)
     if board.save
       flash[:notice] = "「#{board.title}」の掲示板を作成しました"
       redirect_to board
     else
-      redirect_to new_board_path, flash: {
+      redirect_to :back, flash: {
         board: board,
         error_messages: board.errors.full_messages
       }
@@ -27,29 +28,32 @@ class BoardsController < ApplicationController
   end
 
   def edit
+    @board.attributes = flash[:board] if flash[:board]
   end
 
   def update
-    @board.update(board_params)
-
-    redirect_to board
+    if @board.update(board_params)
+      redirect_to @board
+    else
+      redirect_to :back, flash: {
+        board: @board,
+        error_messages: @board.errors.full_messages
+      }
+    end
   end
 
   def destroy
     @board.delete
-
-    redirect_to boards_path, flash: { notice: "「#{@board.title}」の掲示板を削除しました"}
+    redirect_to boards_path, flash: { notice: "「#{@board.title}」の掲示板が削除されました" }
   end
-
 
   private
 
   def board_params
-    params.require(:board).permit(:name, :title, :body)
+    params.require(:board).permit(:name, :title, :body, tag_ids: [])
   end
 
   def set_target_board
     @board = Board.find(params[:id])
   end
-
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -7,7 +7,8 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+
 class Tag < ApplicationRecord
-  has_many :board_tag_relations
+  has_many :board_tag_relations, dependent: :delete_all
   has_many :boards, through: :board_tag_relations
 end
